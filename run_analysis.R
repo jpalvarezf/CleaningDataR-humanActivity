9e30346ac9c17fd1b2aa6b2591ab47fcd9829f37
Juan Pablo Alvarez <jpalvarez@gmail.com>
2015-10-25 13:50:38 -0600

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,66 @@
+#Title: Run Analysis Scripts
+#Autor: Juan Pablo Alvarez
+#Date: 2015-10-23
+
+#Install packages if necessary
+#install.packages("dplyr")
+#install.packages("reshape2")
+
+#Libraries
+library(dplyr)
+library(reshape2)
+
+
+#Obtain TRAIN DATA
+trainFile <- "./train/X_train.txt"
+trainLabelsFile <- "./train/y_train.txt"
+trainSubjectFile <- "./train/subject_train.txt"
+#Main TRAIN DATA file
+train <- read.table(trainFile, header = FALSE, sep = "", quote = "", dec = ".", comment.char = "", colClasses = "numeric")
+#TRAIN labels file
+trainLabels <- read.table(trainLabelsFile, header = FALSE, sep = "", quote = "", dec = ".", comment.char = "", colClasses = "numeric")
+names(trainLabels) <- c("Label")
+train <- bind_cols(trainLabels, train)
+#TRAIN subject file
+trainSubject <- read.table(trainSubjectFile, header = FALSE, sep = "", quote = "", dec = ".", comment.char = "", colClasses = "numeric")
+names(trainSubject) <- c("Subject")
+train <- bind_cols(trainSubject, train)
+
+#Obtain TEST DATA
+testFile <- "./test/X_test.txt"
+testLabelsFile <- "./test/y_test.txt"
+testSubjectFile <- "./test/subject_test.txt"
+#Main TEST DATA file
+test <- read.table(testFile, header = FALSE, sep = "", quote = "", dec = ".", comment.char = "", colClasses = "numeric")
+#TEST labels file
+testLabels <- read.table(testLabelsFile, header = FALSE, sep = "", quote = "", dec = ".", comment.char = "", colClasses = "numeric")
+names(testLabels) <- c("Label")
+test <- bind_cols(testLabels, test)
+#TEST subject file
+testSubject <- read.table(testSubjectFile, header = FALSE, sep = "", quote = "", dec = ".", comment.char = "", colClasses = "numeric")
+names(testSubject) <- c("Subject")
+test <- bind_cols(testSubject, test)
+
+#Bind by rows TRAIN and TEST DATA
+humanActivity <- bind_rows(train, test)
+
+#Obtain features
+haColNamesFile <- "./features.txt"
+haColNames <- read.table(haColNamesFile, header = FALSE, sep = "", quote = "", comment.char = "", stringsAsFactors = FALSE)
+
+#Clean and make more readable the dataset columns names
+haColNames$V2 <- gsub("\\(", "", haColNames$V2)
+haColNames$V2 <- gsub("\\)", "", haColNames$V2)
+haColNames$V2 <- gsub("\\,", "_", haColNames$V2)
+haColNames$V2 <- gsub("\\-", "_", haColNames$V2)
+#Make a unique names
+haColNames$V3 <- paste0(haColNames$V2, "-", haColNames$V1)
+names(humanActivity) <- c("Subject", "Labels", haColNames$V3)
+
+#Replaces Labels codes with readable labels names
+activityNamesFile <- "./activity_labels.txt"
+activityNames <- read.table(activityNamesFile, header = FALSE, sep = "", quote = "", comment.char = "", stringsAsFactors = FALSE)
+humanActivity_use <- humanActivity %>% select(Subject, Labels, contains("mean"), contains("std")) %>% mutate(Labels = plyr::mapvalues(Labels, activityNames$V1, activityNames$V2))
+
+#Obtain the mean of features for each label and each subject
+humanActivity_summarize <- humanActivity_use %>% group_by(Subject, Labels) %>% summarise_each(funs(mean))
